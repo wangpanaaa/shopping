7a4fb09e685d052d3d3df23c09351732e2829be1
wangpan <wangpannnn@gmail.com>
2020-07-30 19:54:45 +0800

    初始化仓库

---
+++ moog/App.vue
@@ -0,0 +1,26 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	@import "colorui/main.css";
+	@import "colorui/icon.css";
+	/*每个页面公共css */
+	.icon {
+	  width: 1em;
+	  height: 1em;
+	  vertical-align: -0.15em;
+	  fill: currentColor;
+	  overflow: hidden;
+	}
+</style>
---
+++ moog/pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view>
+		<view class="bg-img" style="background-image: url('../../static/images/title_bg.png');height: 578upx;">
+			<view class="title">
+				<svg class="icon menu" aria-hidden="true">
+				  <use xlink:href="#icon-mulu"></use>
+				</svg>
+				<text>
+					<svg class="icon" aria-hidden="true">
+					  <use xlink:href="#icon-che"></use>
+					</svg>
+					<text>Shopping</text>
+				</text>
+				<svg class="icon" aria-hidden="true">
+				  <use xlink:href="#icon-kefu"></use>
+				</svg>
+			</view>
+
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.title{
+		color: #fff;
+		.menu {
+			width: 47upx;
+			height: 41upx;
+		}
+	}
+	
+</style>
